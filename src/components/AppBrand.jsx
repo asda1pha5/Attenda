b53fd4b90ae0db49fd72d356fc364854c440bf80
Alrens Antoine <asda1pha5@gmail.com>
2026-08-16 12:51:16 -0400

    Apply Attendaa RSVP wordmark

--- src/components/AppBrand.jsx
+++ src/components/AppBrand.jsx
@@ -1,11 +1,10 @@
 import { Link } from 'react-router-dom';
-import mark from '../assets/attendaa-mark.png';
+import wordmark from '../assets/attendaa-wordmark.png';
 
 export default function AppBrand({ to = '/login?mode=signup', subtle = false }) {
   return (
     <Link className={`app-brand${subtle ? ' app-brand-subtle' : ''}`} to={to} aria-label="Attendaa home">
-      <img src={mark} alt="" />
-      <span>Attendaa</span>
+      <img src={wordmark} alt="" />
     </Link>
   );
 }
